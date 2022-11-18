d52a9f45828fbb986022f218d861013b8065e48a
Ikenna <ikenna@kali.Ikenna-PC>
2022-11-18 00:21:35 -0500

    Delete

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - a function to insert to the right node 
+ * 
+ * @parent: a pointer to the node to insert the right-child in
+ * @value: is the value to store in the new node
+ * @return: pointer to the new node or null on failure
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+
+	if (parent->right == NULL)
+	{
+		parent->right = newNode;
+		newNode->right = NULL;
+	}
+	else
+	{
+		newNode->right = parent->right;
+		parent->right->parent = newNode;
+		parent->right = newNode;
+	}
+	return (newNode);	
+}
